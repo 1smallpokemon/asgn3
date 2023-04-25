34445ae79ca8cd80ef03e4577a3b115ec99b7ad8
Joshua Levin <thejoryle@gmail.com>
2023-04-24 17:18:24 -0700

    binop parse and interp done

--- asgn3.rkt
+++ asgn3.rkt
@@ -2,45 +2,53 @@
 
 (require typed/rackunit)
 
-(define-type ExprC (U NumC BinopC PlusC MultC))
+(define-type ExprC (U NumC BinopC))
 (struct BinopC ([op : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
 (struct NumC ([n : Real]) #:transparent)
-(struct PlusC ([l : ExprC] [r : ExprC])#:transparent)
-(struct MultC ([l : ExprC] [r : ExprC])#:transparent)
 
 ;; hash-table for BinopC, converts binary operators to their corresponding
 ;; racket operation
-(define binop-hash
+(define ops
   (hash
-   PlusC +
-   MultC *))
+   '+ +
+   '* *
+   '- -
+   '/ /))
 
 ;; main VVQS parsing function
 (define (parse [expr : Sexp]) : ExprC
   (match expr
     [(? real? n) (NumC n)]
-    [(list (? symbol? s) l r) (if (hash-has-key? binop-hash s)
+    [(list (? symbol? s) l r) (if (hash-has-key? ops s)
                                   (BinopC s (parse l) (parse r))
-                                  (error  'parse "expected legal expression, got: ~e" s))]
-    [other (error  'parse "expected legal expression, got: ~e" other)]))
+                                  (error 'parse "VVQS: illegal operator ~e" s))]
+    [other (error 'parse "VVQS: illegal expression: ~e" other)]))
 
-(define a1 (PlusC (NumC 1) (NumC 2)))
-(define a2 (PlusC (NumC 3) a1))
-(define a3 (MultC a1 a2))
+(define a1 (BinopC '+ (NumC 1) (NumC 2)))
+(define a2 (BinopC '+ (NumC 3) a1))
+(define a3 (BinopC '* a1 a2))
+(define sub (BinopC '- (NumC 3) (NumC 2)))
+(define div (BinopC '/ (NumC 4) (NumC 2)))
 (check-equal? (parse '(+ 1 2)) a1)
 (check-equal? (parse '(+ 3 (+ 1 2))) a2)
 (check-equal? (parse '(* (+ 1 2) (+ 3 (+ 1 2)))) a3)
-(check-exn #rx"expected" (lambda () (parse '{+ 4})))
+(check-equal? (parse '(- 3 2)) sub)
+(check-equal? (parse '(/ 4 2)) div)
+(check-exn #rx"expression" (lambda () (parse '{+ 4})))
+(check-exn #rx"operator" (lambda () (parse '{& 4 5})))
 
 ;; interp consumes an abstract syntax tree to produce an answer
 (define (interp (ar : ExprC)) : Real
   (match ar
     [(NumC n) n]
+    [(BinopC '/ l (NumC 0)) (error 'interp "VVQS: divide by zero")]
     [(BinopC o l r)
-     ((hash-ref binop-hash o (error 'interp "invalid binop ~e" o))
-                     (interp l) (interp r))]))
+     ((hash-ref ops o) (interp l) (interp r))]))
 
 (check-equal? (interp a1) 3)
 (check-equal? (interp a2) 6)
 (check-equal? (interp a3) 18)
+(check-equal? (interp sub) 1)
+(check-equal? (interp div) 2)
 (check-equal? (interp (NumC 0)) 0)
+(check-exn #rx"zero" (λ () (interp (BinopC '/ (NumC 5) (NumC 0)))))
