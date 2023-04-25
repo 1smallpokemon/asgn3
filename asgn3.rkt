8502054d22a79c3737054012aff76670da47ba11
Joshua Levin <thejoryle@gmail.com>
2023-04-24 19:46:24 -0700

    leq?0 complete

--- asgn3.rkt
+++ asgn3.rkt
@@ -2,9 +2,10 @@
 
 (require typed/rackunit)
 
-(define-type ExprC (U NumC BinopC))
+(define-type ExprC (U NumC BinopC leq0?))
 (struct BinopC ([op : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
 (struct NumC ([n : Real]) #:transparent)
+(struct leq0? ([test : ExprC] [then : ExprC] [else : ExprC]) #:transparent)
 
 ;; hash-table for BinopC, converts binary operators to their corresponding
 ;; racket operation
@@ -22,6 +23,8 @@
     [(list (? symbol? s) l r) (if (hash-has-key? ops s)
                                   (BinopC s (parse l) (parse r))
                                   (error 'parse "VVQS: illegal operator ~e" s))]
+    [(list 'leq0? test then else)
+     (leq0? (parse test) (parse then) (parse else))]
     [other (error 'parse "VVQS: illegal expression: ~e" other)]))
 
 (define a1 (BinopC '+ (NumC 1) (NumC 2)))
@@ -29,6 +32,8 @@
 (define a3 (BinopC '* a1 a2))
 (define sub (BinopC '- (NumC 3) (NumC 2)))
 (define div (BinopC '/ (NumC 4) (NumC 2)))
+(define leq0-1 (leq0? (NumC 1) a1 a2))
+(define leq0-2 (leq0? (NumC -1) a1 a2))
 (check-equal? (parse '(+ 1 2)) a1)
 (check-equal? (parse '(+ 3 (+ 1 2))) a2)
 (check-equal? (parse '(* (+ 1 2) (+ 3 (+ 1 2)))) a3)
@@ -36,6 +41,7 @@
 (check-equal? (parse '(/ 4 2)) div)
 (check-exn #rx"expression" (lambda () (parse '{+ 4})))
 (check-exn #rx"operator" (lambda () (parse '{& 4 5})))
+(check-equal? (parse '(leq0? 1 (+ 1 2) (+ 3 (+ 1 2)))) leq0-1)
 
 ;; interp consumes an abstract syntax tree to produce an answer
 (define (interp (ar : ExprC)) : Real
@@ -43,7 +49,10 @@
     [(NumC n) n]
     [(BinopC '/ l (NumC 0)) (error 'interp "VVQS: divide by zero")]
     [(BinopC o l r)
-     ((hash-ref ops o) (interp l) (interp r))]))
+     ((hash-ref ops o) (interp l) (interp r))]
+    [(leq0? test then else) (if (<= (interp test) 0)
+                                (interp then)
+                                (interp else))]))
 
 (check-equal? (interp a1) 3)
 (check-equal? (interp a2) 6)
@@ -52,3 +61,5 @@
 (check-equal? (interp div) 2)
 (check-equal? (interp (NumC 0)) 0)
 (check-exn #rx"zero" (λ () (interp (BinopC '/ (NumC 5) (NumC 0)))))
+(check-equal? (interp leq0-1) 6)
+(check-equal? (interp leq0-2) 3)
