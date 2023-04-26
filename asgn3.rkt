2aea40801c75ca5a9bfb82df79cb8676ecae5fa0
Joseph Dewar <superjoeyd123@gmail.com>
2023-04-25 18:56:51 -0700

    final commit before merge

--- asgn3.rkt
+++ asgn3.rkt
@@ -31,28 +31,25 @@
             ['= #f]
             [other #t])]))
 
-(define (parse-fundef [s : Sexp]) : FundefC
-  (match s
-    [(list 'def (list (? symbol? (? ValidSymbol? id))
-                      (? symbol? (? ValidSymbol? arg))) '= exp)
-     (FunC id arg (parse exp))]
-    [other (error 'parse-fundef "VVQS: illegal function ~e" s)]))
-
 (define (parse-prog [s : Sexp]) : (Listof FundefC)
   (map parse-fundef (cast s (Listof Sexp))))
 
-(define (lookup-fun (name : Symbol) (funs : (Listof FundefC))) : FundefC
-  (cond [(empty? funs) (error 'interp "VVQS: function not found ~e" name)]
-        [else
-         (define f (car funs))
-         (cond [(FunC? f) (if (symbol=? name (FunC-name f)) f (lookup-fun name (cdr funs)))]
-               [else (lookup-fun name (cdr funs))])]))
+(: top-interp (Sexp -> Real))
+(define (top-interp fun-sexps)
+  (interp-fns (parse-prog fun-sexps)))
 
 (define (interp-fns [funs : (Listof FundefC)]) : Real
   (define main (lookup-fun 'main funs))
   (define init (NumC 0))
-  (interp (FunC-body main) funs))
+  (define main-body-substituted (subst 'init init (FunC-body main)))
+  (interp main-body-substituted funs))
 
+(define (lookup-fun (name : Symbol) (funs : (Listof FundefC))) : FundefC
+  (cond [(empty? funs) (error 'interp "VVQS: function not found ~e" name)]
+        [else
+         (define f (first funs))
+         (cond [(FunC? f) (if (symbol=? name (FunC-name f)) f (lookup-fun name (cdr funs)))]
+               [else (lookup-fun name (cdr funs))])]))
 
 ;; main VVQS parsing function
 (define (parse [expr : Sexp]) : ExprC
@@ -61,38 +58,19 @@
     [(list (? symbol? s) l r) (if (hash-has-key? ops s)
                                   (BinopC s (parse l) (parse r))
                                   (error 'parse "VVQS: illegal operator ~e" s))]
-    [(list 'leq0? test then else)
+    [(list 'leq0? test 'then then 'else else)
      (leq0? (parse test) (parse then) (parse else))]
     [(? symbol? (? ValidSymbol? id)) (IdC id)]
     [(list (? symbol? (? ValidSymbol? f)) arg)
      (FunAppC f (parse arg))]
     [other (error 'parse "VVQS: illegal expression: ~e" other)]))
 
-(define a1 (BinopC '+ (NumC 1) (NumC 2)))
-(define a2 (BinopC '+ (NumC 3) a1))
-(define a3 (BinopC '* a1 a2))
-(define sub (BinopC '- (NumC 3) (NumC 2)))
-(define div (BinopC '/ (NumC 4) (NumC 2)))
-(define leq0-1 (leq0? (NumC 1) a1 a2))
-(define leq0-2 (leq0? (NumC -1) a1 a2))
-(check-equal? (parse '(+ 1 2)) a1)
-(check-equal? (parse '(+ 3 (+ 1 2))) a2)
-(check-equal? (parse '(* (+ 1 2) (+ 3 (+ 1 2)))) a3)
-(check-equal? (parse '(* 3 (- 1 2))) (BinopC '* (NumC 3) (BinopC '- (NumC 1) (NumC 2))))
-(check-equal? (parse '(- 3 2)) sub)
-(check-equal? (parse '(/ 4 2)) div)
-(check-exn #rx"expression" (lambda () (parse '{+ 4})))
-(check-exn #rx"operator" (lambda () (parse '{& 4 5})))
-(check-equal? (parse '(leq0? 1 (+ 1 2) (+ 3 (+ 1 2)))) leq0-1)
-(check-equal? (parse '(leq0? 1 2 3)) (leq0? (NumC 1) (NumC 2) (NumC 3)))
-(check-equal? (parse 'x) (IdC 'x))
-(check-equal? (parse '(f 5)) (FunAppC 'f (NumC 5)))
-
-; Test cases for function parsing
-(check-equal? (parse-fundef '{def {add x} = {+ x 1}}) (FunC 'add 'x (BinopC '+ (IdC 'x) (NumC 1))))
-(check-equal? (parse-fundef '{def {mul x} = {* x 2}}) (FunC 'mul 'x (BinopC '* (IdC 'x) (NumC 2))))
-
-
+(define (parse-fundef [s : Sexp]) : FundefC
+  (match s
+    [(list 'def (list (? symbol? (? ValidSymbol? id))
+                      (? symbol? (? ValidSymbol? arg))) '= exp)
+     (FunC id arg (parse exp))]
+    [other (error 'parse-fundef "VVQS: illegal function ~e" s)]))
 
 ;; interp consumes an abstract syntax tree to produce an answer
 (define (interp [exp : ExprC] [funs : (Listof FundefC)]) : Real
@@ -110,10 +88,6 @@
      (define substituted-body (subst (FunC-arg fun-def) (NumC arg-val) (FunC-body fun-def)))
      (interp substituted-body funs)]))
 
-(: top-interp (Sexp -> Real))
-(define (top-interp fun-sexps)
-  (interp-fns (parse-prog fun-sexps)))
-
 (define (subst (x : Symbol) (v : ExprC) (e : ExprC)) : ExprC
   (match e
     [(NumC _) e]
@@ -121,6 +95,32 @@
     [(BinopC o l r) (BinopC o (subst x v l) (subst x v r))]
     [(leq0? test then else) (leq0? (subst x v test) (subst x v then) (subst x v else))]
     [(FunAppC fun arg) (FunAppC fun (subst x v arg))]))
+
+; Test cases for parsing
+(define a1 (BinopC '+ (NumC 1) (NumC 2)))
+(define a2 (BinopC '+ (NumC 3) a1))
+(define a3 (BinopC '* a1 a2))
+(define sub (BinopC '- (NumC 3) (NumC 2)))
+(define div (BinopC '/ (NumC 4) (NumC 2)))
+(define leq0-1 (leq0? (NumC 1) a1 a2))
+(define leq0-2 (leq0? (NumC -1) a1 a2))
+(check-equal? (parse '(+ 1 2)) a1)
+(check-equal? (parse '(+ 3 (+ 1 2))) a2)
+(check-equal? (parse '(* (+ 1 2) (+ 3 (+ 1 2)))) a3)
+(check-equal? (parse '(* 3 (- 1 2))) (BinopC '* (NumC 3) (BinopC '- (NumC 1) (NumC 2))))
+(check-equal? (parse '(- 3 2)) sub)
+(check-equal? (parse '(/ 4 2)) div)
+(check-exn #rx"expression" (lambda () (parse '{+ 4})))
+(check-exn #rx"operator" (lambda () (parse '{& 4 5})))
+(check-equal? (parse '(leq0? 1 then (+ 1 2) else (+ 3 (+ 1 2)))) leq0-1)
+(check-equal? (parse '(leq0? 1 then 2 else 3)) (leq0? (NumC 1) (NumC 2) (NumC 3)))
+(check-equal? (parse 'x) (IdC 'x))
+(check-equal? (parse '(f 5)) (FunAppC 'f (NumC 5)))
+
+; Test cases for function parsing
+(check-equal? (parse-fundef '{def {add x} = {+ x 1}}) (FunC 'add 'x (BinopC '+ (IdC 'x) (NumC 1))))
+(check-equal? (parse-fundef '{def {mul x} = {* x 2}}) (FunC 'mul 'x (BinopC '* (IdC 'x) (NumC 2))))
+
 
 ; Test cases for program parsing and interpretation
 (define test-prog1
@@ -133,6 +133,15 @@
     {def {div x} = {/ x 2}}
     {def {main init} = {sub {div init}}}})
 
+(check-equal? (top-interp test-prog1) 1) ; (add (mul 0)) = (add 0) = 1
+(check-equal? (top-interp test-prog2) -1) ; (sub (div 0)) = (sub 0) = -1
+
+; Test cases for conditional expressions
+(define test-prog3
+  '{{def {negate x} = {leq0? x then {- 0 x} else {* -1 x}}}
+    {def {main init} = {negate {+ init 1}}}})
+
+(check-equal? (top-interp test-prog3) -1) ; (negate (+ 0 1)) = (negate 1) = -1
 
 
 (check-equal? (interp a1 '()) 3)
